a18d850422b1d901ce8bae2b7b95905681fcb5d6
talha <talhajubaer018@gmail.com>
2019-11-13 16:28:40 +0600

    express route

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.get('/', function (req, res) {
-    res.render('test', { title: 'Hey', message: 'Hello there!' })
+    res.render('home', { title: 'Hey', message: 'Hello there!' })
+})
+app.get('/about', function (req, res) {
+    res.render('simple', { title: 'About Us', message: 'About' })
+})
+app.get('/contact', function (req, res) {
+    res.render('simple', { title: 'Contact Us', message: 'Contact' })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
