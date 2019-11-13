28f9cee432233bfd0037d054d14697c08b6b85b0
talha <talhajubaer018@gmail.com>
2019-11-13 18:03:12 +0600

    css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loaders: ['babel-loader']
-            }
+            },
+            {
+                test: /\.scss$/i,
+                loaders: ['css-loader']
+            },
         ]
     }
 };
