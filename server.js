356a9197e74c819de1877c2f1c235c6cd4c92016
talha <talhajubaer018@gmail.com>
2019-11-13 15:23:37 +0600

    html to pug

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 80
 
 app.use(express.static('public'))
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.get('/', function (req, res) {
+    res.render('test', { title: 'Hey', message: 'Hello there!' })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
