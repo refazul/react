09ea4b6387ebd7ece0ec100993a69d3c32bdcac2
refazul <refazul.refat@yahoo.com>
2019-11-25 16:01:19 +0600

    sidebar cleanup

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,15 +8,8 @@ import Sidebarmenu from "./sidebarmenu";
 export default class Menu extends Component {
 	render() {
 		return (
-			<div className="row">
-				<div className="col-lg-2 col-md-2 col-sm-2">
-					<Sidebarmenu {...sidebarmenu_data} />
-				</div>
-				<div className="col-lg-10 col-md-10 col-sm-10">
-					<div id="cssmenu">
-						<Navigation {...navigation_data} />
-					</div>
-				</div>
+			<div>
+				<Sidebarmenu {...sidebarmenu_data} />
 			</div>
 		);
 	}
--- src/js/sidebarmenu.js
+++ src/js/sidebarmenu.js
@@ -29,7 +29,6 @@ const Menu = (props) => {
 					})
 				}
 			</ul>
-
 		</li>
 	)
 };
@@ -37,16 +36,12 @@ const Menu = (props) => {
 const Sidebarmenu = (props) => {
 	return (
 		<aside className="main-sidebar sidebar-dark-primary elevation-4">
-
 			<a href="index3.html" className="brand-link">
 				<img src="img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3"
 					style={{ opacity: .8 }} />
 				<span className="brand-text font-weight-light">AdminLTE 3</span>
 			</a>
-
-
 			<div className="sidebar">
-
 				<div className="user-panel mt-3 pb-3 mb-3 d-flex">
 					<div className="image">
 						<img src="img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
@@ -55,11 +50,8 @@ const Sidebarmenu = (props) => {
 						<a href="#" className="d-block">Alexander Pierce</a>
 					</div>
 				</div>
-
-
 				<nav className="mt-2">
 					<ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
 						{
 							props.items.map((item) => {
 								return <Menu {...item} />
@@ -67,11 +59,8 @@ const Sidebarmenu = (props) => {
 						}
 					</ul>
 				</nav>
-
 			</div>
-
 		</aside>
-
 	);
 }
 
