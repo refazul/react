ed56823d9a00d617df7041c40c8abae12e178fc9
talha <talhajubaer018@gmail.com>
2019-12-28 19:30:41 +0600

    navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,71 +1,85 @@
 import React, { useState, useRef, useEffect } from 'react';
-import $ from 'jquery';
+
 // Very important
 
 const Item = (props) => {
 	var items = props.items || [];
-	var toplevel = props.toplevel;
-	const [count, setCount] = useState(0);
-	const dropdownref = useRef(null);
+	var li_class_name = "";
+	var ul_class_name = " hidden ";
+	var dd_trigger_class_name = "";
+	var dd_arrow_class_name = "";
 	useEffect(() => {
 		// Multiple Times
-		if (count % 2 == 0) {
-			$(dropdownref.current).removeClass('open');
-		} else {
-			$(dropdownref.current).addClass('open');
-		}
+
 	});
 	useEffect(() => {
 		// Just Once
 	}, []);
-	function dropdownOnclick(e) {
-		setCount(count + 1);
+
+	if (items.length > 0) {
+		li_class_name += " cn-dropdown-item has-down pr12 ";
+		ul_class_name += " dropdown "
+		dd_trigger_class_name += " dd-trigger "
+		dd_arrow_class_name += " dd-arrow "
 	}
 	return (
-		<li className={toplevel & items.length > 0 ? "dropdown" : (items.length > 0 ? "dropdown-submenu" : "")} ref={dropdownref} onMouseEnter={dropdownOnclick} onMouseLeave={dropdownOnclick}>
-			<a  href="#" className="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-				{props.text}
-				{
-					toplevel && items.length > 0 ? <b className="caret"></b> : <b></b>
-				}
-			</a>
-			<ul className={items.length > 0 ? "dropdown-menu" : ""}>
+		<li class={li_class_name}><a href="#">{props.text}</a>
+			<ul class={ul_class_name}>
 				{
 					items.map((item) => {
 						return <Item {...item} />
 					})
 				}
 			</ul>
+			<span class={dd_trigger_class_name}></span><span class={dd_arrow_class_name}></span>
 		</li>
 	);
 }
 
 
 const Navbar = (props) => {
-	var items = props.items;
+	var items = props.items || [];
 	return (
-		<div>
-			<nav className="navbar navbar-default" role="navigation">
-				<div className="navbar-header">
-					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-						<span className="sr-only">Toggle navigation</span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-					</button>
-					<a className="navbar-brand" href="#">Brand</a>
-				</div>
+		<div id="stickyNav-sticky-wrapper" class="sticky-wrapper" style={{ height: "100px" }}>
+			<div class="original-nav-area" id="stickyNav">
+				<div class="classy-nav-container breakpoint-off light left">
+					<div class="container">
 
-				<div className="collapse navbar-collapse navbar-ex1-collapse">
-					<ul className="nav navbar-nav">
-						{
-							items.map((item) => {
-								return <Item {...item} toplevel={true} />
-							})
-						}
-					</ul>
+						<nav class="classy-navbar justify-content-between">
+							<div class="classy-navbar-toggler">
+								<span class="navbarToggler"><span></span><span></span><span></span></span>
+							</div>
+							<div class="classy-menu" id="originalNav">
+
+								<div class="classycloseIcon">
+									<div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+								</div>
+								<div class="classynav">
+									<ul>
+										{
+											items.map((item) => {
+												return <Item {...item} />
+											})
+										}
+									</ul>
+
+								</div>
+
+							</div>
+							<form class="form-inline ml-3 ">
+								<div class="input-group input-group-sm">
+									<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" />
+									<div class="input-group-append">
+										<button class="btn btn-navbar" type="submit">
+											<i class="fas fa-search"></i>
+										</button>
+									</div>
+								</div>
+							</form>
+						</nav>
+					</div>
 				</div>
-			</nav>
+			</div>
 		</div>
 
 	);
---
+++ src/js/navbarbootstrap3.js
@@ -0,0 +1,74 @@
+import React, { useState, useRef, useEffect } from 'react';
+import $ from 'jquery';
+// Very important
+// bootstrap 3.3.7
+
+const Item = (props) => {
+	var items = props.items || [];
+	var toplevel = props.toplevel;
+	const [count, setCount] = useState(0);
+	const dropdownref = useRef(null);
+	useEffect(() => {
+		// Multiple Times
+		if (count % 2 == 0) {
+			$(dropdownref.current).removeClass('open');
+		} else {
+			$(dropdownref.current).addClass('open');
+		}
+	});
+	useEffect(() => {
+		// Just Once
+	}, []);
+	function dropdownOnclick(e) {
+		setCount(count + 1);
+	}
+	return (
+		<li className={toplevel & items.length > 0 ? "dropdown" : (items.length > 0 ? "dropdown-submenu" : "")} ref={dropdownref} onMouseEnter={dropdownOnclick} onMouseLeave={dropdownOnclick}>
+			<a  href="#" className="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+				{props.text}
+				{
+					toplevel && items.length > 0 ? <b className="caret"></b> : <b></b>
+				}
+			</a>
+			<ul className={items.length > 0 ? "dropdown-menu" : ""}>
+				{
+					items.map((item) => {
+						return <Item {...item} />
+					})
+				}
+			</ul>
+		</li>
+	);
+}
+
+
+const Navbar = (props) => {
+	var items = props.items;
+	return (
+		<div>
+			<nav className="navbar navbar-default" role="navigation">
+				<div className="navbar-header">
+					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span className="sr-only">Toggle navigation</span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+					</button>
+					<a className="navbar-brand" href="#">Brand</a>
+				</div>
+
+				<div className="collapse navbar-collapse navbar-ex1-collapse">
+					<ul className="nav navbar-nav">
+						{
+							items.map((item) => {
+								return <Item {...item} toplevel={true} />
+							})
+						}
+					</ul>
+				</div>
+			</nav>
+		</div>
+
+	);
+}
+export default Navbar;
