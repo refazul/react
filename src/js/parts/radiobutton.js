8559458f9df29438eab6f6c6615d25b7fe751a45
talha <talhajubaer018@gmail.com>
2019-12-31 16:38:19 +0600

    radiobutton (fixed)

--- src/js/parts/radiobutton.js
+++ src/js/parts/radiobutton.js
@@ -2,25 +2,13 @@ import React, { useState, useRef, useEffect } from 'react';
 
 
 export const RadioButton = (props) => {
-    var wrap_class = "";
-    var inputwrap_class = "";
 
-    if (props.orientation == 'horizontal') {
-        wrap_class = " row ";
-        inputwrap_class = " offset-sm-4 col-sm-8 ";
-    }
-    if (props.boxes == 'checkboxes') {
-        wrap_class = " row ";
-        inputwrap_class = " col-sm-6 ";
-    }
 
     return (
-        <div class={"form-group" + wrap_class}>
-            <div class={inputwrap_class}>
-                <div class="form-check">
-                    <input type="radio" class="form-check-input" id={props.id} />
-                    <label class="form-check-label" for={props.for}>{props.text}</label>
-                </div>
+        <div class={"form-group"}>
+            <div class="form-check">
+                <input type="radio" class="form-check-input" id={props.id} name={props.name}/>
+                <label class="form-check-label" for={props.id}>{props.text}</label>
             </div>
         </div>
     )
