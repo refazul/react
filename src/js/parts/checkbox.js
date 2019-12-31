c411383840d1e511026aefa827cedc5c7fca0c1d
talha <talhajubaer018@gmail.com>
2019-12-31 16:51:29 +0600

    checkbox horizontal

--- src/js/parts/checkbox.js
+++ src/js/parts/checkbox.js
@@ -2,11 +2,20 @@ import React, { useState, useRef, useEffect } from 'react';
 
 
 export const CheckBox = (props) => {
+    var wrap_class = "";
+    var inputwrap_class = "";
+
+    if (props.orientation == 'horizontal') {
+        wrap_class = " row ";
+        inputwrap_class = " offset-sm-4 col-sm-8 ";
+    }
     return (
-        <div class={"form-group"}>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id={props.id} />
-                <label class="form-check-label" for={props.id}>{props.text}</label>
+        <div class={"form-group" + wrap_class}>
+            <div className={inputwrap_class}>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id={props.id} />
+                    <label class="form-check-label" for={props.id}>{props.text}</label>
+                </div>
             </div>
         </div>
     )
