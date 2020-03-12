afa50cdfc5f8bff2d10dadc8752b1d19c95ed79d
talha <talhajubaer018@gmail.com>
2020-03-12 17:01:42 +0600

    first table prototype

--- crawler.js
+++ crawler.js
@@ -3,36 +3,37 @@ const cheerio = require('cheerio');
 const url = require('url');
 
 function court_scan(param = {}) {
-    var date = param.date;
-    var court_link = 'http://supremecourt.gov.bd/web/?page=bench_list.php&menu=00&div_id=2' + '&date1=' + date;
-    var court_data = [];
-    var promises = [];
-    axios.get(court_link).then((response) => {
-        const $ = cheerio.load(response.data);
+    return new Promise((resolve, reject) => {
+        var date = param.date;
+        var court_link = 'http://supremecourt.gov.bd/web/?page=bench_list.php&menu=00&div_id=2' + '&date1=' + date;
+        var court_data = [];
+        var promises = [];
+        axios.get(court_link).then((response) => {
+            const $ = cheerio.load(response.data);
 
-        var table = $('form[name="form1"]').siblings('table');
-        var tr = $(table).children().children().not(':first-child');
-        $(tr).each(function () {
-            var row = this;
+            var table = $('form[name="form1"]').siblings('table');
+            var tr = $(table).children().children().not(':first-child');
+            $(tr).each(function () {
+                var row = this;
 
-            var c0 = $(row).children('td').eq(0);
-            var c1 = $(row).children('td').eq(1);
-            var c2 = $(row).children('td').eq(2);
-            var c3 = $(row).children('td').eq(3);
+                var c0 = $(row).children('td').eq(0);
+                var c1 = $(row).children('td').eq(1);
+                var c2 = $(row).children('td').eq(2);
+                var c3 = $(row).children('td').eq(3);
 
-            var court_name = $(c1).find('strong a').text();
-            var judge_name = extract_text($, c2);
-            var court_link = $(c1).find('strong a').attr('href');
-            var court_id = url.parse(court_link, { parseQueryString: true }).query.court_id;
-            var bench_id = url.parse(court_link, { parseQueryString: true }).query.bench_id;
+                var court_name = $(c1).find('strong a').text();
+                var judge_name = extract_all_text($, c2);
+                var court_link = $(c1).find('strong a').attr('href');
+                var court_id = url.parse(court_link, { parseQueryString: true }).query.court_id;
+                var bench_id = url.parse(court_link, { parseQueryString: true }).query.bench_id;
 
-            promises.push(cause_scan({ court_id, bench_id, date, court_name, judge_name }));
+                promises.push(cause_scan({ court_id, bench_id, date, court_name, judge_name }));
+            });
+            Promise.all(promises).then((values) => {
+                resolve(values[0]);
+            });
+
         });
-        Promise.all(promises).then((values) => {
-            //
-            console.log(values[0]);
-        });
-
     });
 }
 
@@ -58,11 +59,18 @@ function cause_scan(param = {}) {
                     var prev = $(row).prev();
                     category = $(prev).text();
                 } else if (valid_row_is($, row)) {
-                    var serial = $(row).children('td').eq(0).text().trim();
-                    var case_number = $(row).children('td').eq(1).text().replace(/\n/g, "").replace(/Petition/i, "Petition ").trim();
-                    var parties = $(row).children('td').eq(2).text().replace(/vs/i, " vs ").trim();
+                    var c0 = $(row).children('td').eq(0);
+                    var c1 = $(row).children('td').eq(1);
+                    var c2 = $(row).children('td').eq(2);
+                    var c3 = $(row).children('td').eq(3);
 
-                    cause_data.push({ serial, case_number, parties, category, court_name, judge_name });
+                    var serial = $(c0).text().trim();
+                    var case_type = extract_nth_text($, c1, 0);
+                    var case_number = extract_nth_text($, c1, 1);
+                    var with_numbers = extract_nth_text($, c1, 2).match(/[0-9 ]+\/[0-9 ]+/gi);
+                    var parties = $(c2).text().replace(/vs/i, " vs ").trim();
+
+                    cause_data.push({ serial, case_type, case_number, with_numbers, date, court_name, judge_name, category, parties });
                 }
             });
             resolve(cause_data);
@@ -70,12 +78,26 @@ function cause_scan(param = {}) {
     })
 }
 
-function extract_text($, el) {
+function extract_all_text($, el) {
     var text = [];
     for (var i = 0; i < $(el).contents().length; i++) {
         var content = $(el).contents()[i];
         if (content.type == 'text') {
             text.push(content.data);
+        }
+    }
+    return text.join(' ').replace(/\s\s+/g, ' ').trim();
+}
+function extract_nth_text($, el, n) {
+    var text = [];
+    for (var i = 0, j = 0; i < $(el).contents().length; i++) {
+        var content = $(el).contents()[i];
+        if (content.type == 'text') {
+            if (j == n) {
+                text.push(content.data);
+                break;
+            }
+            j++;
         }
     }
     return text.join(' ').replace(/\s\s+/g, ' ').trim();
@@ -87,8 +109,11 @@ function sl_row_is($, row) {
     return $(row).children('td').eq(0).text().toLowerCase() == 'sl' ? true : false;
 }
 
-court_scan({ date: '03/03/2020' });
+//court_scan({ date: '03/03/2020' });
 
+module.exports = {
+    court_scan
+}
 
 
 
--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+var court_scan = require('./crawler').court_scan;
+
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.get('/slick', function (req, res) {
     res.render('slick', { title: 'Slick Carousel', message: 'Slick Carousel' })
+})
+app.get('/law', function (req, res) {
+    court_scan({ date: '03/03/2020' }).then((values) => {
+        res.render('table', { title: 'Hey', values: values })
+    })
 })
 app.get('*', function (req, res) {
     res.render('home', { title: 'Hey', message: 'Hello there!' })
