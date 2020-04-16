edbd312e8c091d2eb315fa44ff477417bedf2107
talha <talhajubaer018@gmail.com>
2020-04-16 20:49:37 +0600

    framer proto

--- src/js/framer.js
+++ src/js/framer.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Frame } from "framer";
+import Carousel from "./carousel";
+import Sidebarmenu from "./sidebarmenu";
 
+import { sidebarmenu_data } from './data';
+
+function onTap(x, rotate, event, info) {
+	console.log(info.point.x, info.point.y);
+	return `rotate(${rotate}deg) translateX(${x}px)`
+  }
 export const Framer = (props) => {
 	return (
 		<Frame
-			name={"Image"}
-			scale={0.25}
-			center
-			size={480}
-			image={"images/0.jpg"}
-			drag={"x","y"}
-			dragElastic={0}
-			dragMomentum={false}
-		/>
+			drag={true}
+			size={550}
+			radius={30}
+			background={"#fff"}
+			x={100}
+			y={100}
+			rotate={0}
+			onTap={onTap}
+		>
+			<Carousel />
+			<Sidebarmenu {...sidebarmenu_data} />
+		</Frame>
 	)
 }
